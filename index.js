558a44e87b42b1ad4179ddb8b74e3ec2eef734a9
Luis <luis9609@gmail.com>
2022-02-22 22:21:05 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue <= 400) {
+    return "This one is on me!"
+  }
+  if (someValue > 2500) {
+    return "No can do."
+  }
+  if (someValue > 2000) {
+    return "I will gladly take your thirty bucks."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(City){
   // Write your code here!
+return City == "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === "generous") {
+    return "Thank you so much.";
+  }
+  else if (tip === "not as generous") {
+    return "Thank you.";
+  }
+  else if (tip = "anything else") {
+    return "Bye.";
+  }
+  
 }
